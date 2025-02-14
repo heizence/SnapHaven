d37b6dced0f37c0aaa507f21bd4797482dd933ee
doheon <heizence6626@gmail.com>
2025-02-14 21:21:37 +0900

    edit main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,26 +2,20 @@
  * layout.tsx defines the layout structure shared across all pages in the app
  * It wraps the content of every page using the children prop, enabling consistent headers, footers, or styling across the app.
  */
+import "./globals.css";
+import Navbar from "./Navbar";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   // The <html> tag sets the language attribute (lang="en" in this example), which is important for accessibility and SEO.
   return (
     <html lang="en">
       {/* The children prop is a special React feature. It represents the components rendered within this layout. */}
-      
-      <body style={{padding:0, margin:0}}>
-      <header style={{ background: "#333", color: "#fff", padding: "1rem" }}>
-          <h1>My Website</h1>
-        </header>
-      {children}
-      <footer style={{ background: "#333", color: "#fff", padding: "1rem", marginTop: "2rem" }}>
-    <p>© 2025 My Website</p>
-  </footer>
+
+      <body style={{ padding: 0, margin: 0 }}>
+        <Navbar />
+
+        <div style={{ paddingTop: 31 }}>{children}</div>
       </body>
     </html>
-  )
+  );
 }
