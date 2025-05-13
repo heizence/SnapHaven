e66012b1952c99b93a6b53701b3602d8d0ade71c
doheon <heizence6626@gmail.com>
2025-05-13 17:39:36 +0900

    Add new interfaces

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -24,11 +24,15 @@ export interface CheckEmailResponse {
   token: string;
 }
 
-export interface UploadFileRequest {
-  files: any;
+export interface ResetPasswordRequest {
+  email: string;
 }
 
-export interface UploadFileResponse {
+export interface ResetPasswordResponse {
+  token: string;
+}
+
+export interface UploadFileRequest {
   files: any;
 }
 
@@ -39,8 +43,29 @@ export interface CommonResDto<T> {
   data: T;
 }
 
+export interface SendMailRequest {
+  from: string;
+  to: string;
+  subject: string;
+  text?: string;
+  html: string;
+}
+
+export interface checkResetPwInfoRequest {
+  email: string;
+  token: string;
+}
+
 export interface User {
   id: number;
   email: string;
   password: string;
+}
+
+export interface ResetPasswordRecord {
+  userId: number;
+  email: string;
+  token: string;
+  expiresAt: string;
+  plainPassword: string;
 }
