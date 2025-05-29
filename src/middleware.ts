051359bf5f9cb3d4a0b9ae5d9688cd9b9fe77bf4
doheon <heizence6626@gmail.com>
2025-05-29 18:49:51 +0900

    add token validation to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,45 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { verifyToken } from "./lib/Jwt";
 
-export function middleware(req: NextRequest) {
-  const authToken = req.cookies.get("authToken")?.value;
-  console.log(`\n############# /src/app middleware`);
+export async function middleware(req: NextRequest) {
+  const authToken = req.cookies.get("authToken")?.value || "";
+  const refreshToken = req.cookies.get("refreshToken")?.value || "";
+
+  console.log(`\n### /src/app middleware`);
+
+  const isAuthTokenValid = Boolean(await verifyToken(authToken));
+  const isSignedIn = Boolean(isAuthTokenValid && refreshToken);
+  const isSignedOut = Boolean(!isAuthTokenValid && !refreshToken);
+  const isTokenExpired = Boolean(!isAuthTokenValid && refreshToken);
+
+  const isPathStartsWith = (path: string) => req.nextUrl.pathname.startsWith(path);
 
   // handle /signin page
-  if (authToken && req.nextUrl.pathname.startsWith("/signin")) {
+  if (isSignedIn && isPathStartsWith("/signin")) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
-  // handle /upload page
-  if (!authToken && req.nextUrl.pathname.startsWith("/upload")) {
-    return NextResponse.redirect(new URL("/signin", req.url));
+  if (isSignedOut) {
+    if (isPathStartsWith("/upload") || isPathStartsWith("/myprofile")) {
+      return NextResponse.redirect(new URL("/signin", req.url));
+    }
   }
 
-  // handle /upload page
-  if (req.nextUrl.pathname.startsWith("/api")) {
+  if (req.nextUrl.pathname.startsWith("/api/auth")) {
     const res = NextResponse.next();
 
     res.headers.set("Access-Control-Allow-Origin", "*");
     res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-    if (req.method === "OPTIONS") {
-      return new Response(null, { status: 200 });
+    if (isTokenExpired) {
+      console.log(`## ${req.nextUrl.pathname} 401 error!!!`);
+      return new Response("", { status: 401 });
     }
 
-    if (req.nextUrl.pathname.includes("/api/auth") && !authToken) {
-      return new Response(null, { status: 401 });
+    if (req.method === "OPTIONS") {
+      return new Response(null, { status: 200 });
     }
   }
 
@@ -37,5 +48,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/signin", "/upload", "/api/:path*"],
+  matcher: ["/signin", "/upload", "/myprofile/:path*", "/api/:path*"],
 };
