ce69a9c591f1861081d3f99c9cee38cce3c06836
doheon <heizence6626@gmail.com>
2025-11-13 21:10:14 +0900

    ModalProvider 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
 import ScrollRestoration from "@/components/ScrollRestoration";
 import "./globals.css";
 import Navbar from "../components/Navbar/Navbar.server";
+import { ModalProvider } from "@/contexts/ModalProvider"; // [신규] 1. ModalProvider 임포트
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body style={{ padding: 0, margin: 0 }}>
-        <ScrollRestoration />
-        <Navbar />
-        <div className="container mx-auto pt-[56px]">{children}</div>
+        <ModalProvider>
+          <ScrollRestoration />
+          <Navbar />
+          <div className="container mx-auto pt-[56px]">{children}</div>
+        </ModalProvider>
       </body>
     </html>
   );
