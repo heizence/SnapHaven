9210de1e5f62672a61c96895605dcef6eb695b59
doheon <heizence6626@gmail.com>
2025-12-04 16:42:14 +0900

    (client) refactor : 로그안, 로그아웃 처리 로직 수정

--- client/src/components/Navbar/Navbar.server.tsx
+++ client/src/components/Navbar/Navbar.server.tsx
@@ -2,9 +2,15 @@ import NavbarClient from "./Navbar.client";
 import { cookies } from "next/headers";
 
 export default async function Navbar() {
-  // read the httpOnly “token” cookie on the server
   const Cookies = await cookies();
   const accessToken = Cookies.get("accessToken")?.value || "";
+  const refreshToken = Cookies.get("refreshToken")?.value || "";
 
-  return <NavbarClient accessToken={accessToken} />;
+  /*
+  accessToken 이 만료되어도 refreshToken 으로 토큰 갱신 요청이 가능하기 떄문에 로그인된 상태로 간주.
+  로그아웃 하면 accessToken 과 refreshToken 이 모두 삭제된다.
+  */
+  const isSignedIn = Boolean(accessToken || refreshToken);
+
+  return <NavbarClient isSignedIn={isSignedIn} />;
 }
--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -5,7 +5,11 @@ export async function middleware(req: NextRequest) {
   const accessToken = req.cookies.get("accessToken")?.value || "";
   const refreshToken = req.cookies.get("refreshToken")?.value || "";
 
-  const isSignedIn = Boolean(accessToken && refreshToken);
+  /*
+  accessToken 이 만료되어도 refreshToken 으로 토큰 갱신 요청이 가능하기 떄문에 로그인된 상태로 간주.
+  로그아웃 하면 accessToken 과 refreshToken 이 모두 삭제된다.
+  */
+  const isSignedIn = Boolean(accessToken || refreshToken);
   const isSignedOut = Boolean(!accessToken && !refreshToken);
 
   const isPathStartsWith = (path: string) => req.nextUrl.pathname.startsWith(path);
@@ -14,7 +18,6 @@ export async function middleware(req: NextRequest) {
   if (isSignedIn && isPathStartsWith("/signin")) {
     return NextResponse.redirect(new URL("/", req.url));
   }
-
   if (isSignedOut) {
     if (isPathStartsWith("/upload") || isPathStartsWith("/mypage")) {
       return NextResponse.redirect(new URL("/signin", req.url));
