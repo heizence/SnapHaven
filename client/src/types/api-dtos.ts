ceddcb175247e17ffffff090d4ea557832534610
doheon <heizence6626@gmail.com>
2026-02-11 17:56:34 +0900

    (client) refactor : MediaItem 관련 dto 수정

--- client/src/types/api-dtos.ts
+++ client/src/types/api-dtos.ts
@@ -165,7 +165,7 @@ export type Tag = {
 
 // 각 미디어 아이템 형식
 type MediaItemDto = {
-  id: number;
+  id: string;
   title: string;
   type: ContentType.IMAGE | ContentType.VIDEO;
   status: ContentStatus;
@@ -178,15 +178,19 @@ type MediaItemDto = {
   keyVideoPreview: string | null;
   keyVideoPlayback: string | null;
   isLikedByCurrentUser: boolean;
+  likeCount: number;
+  createdAt: string;
 };
 
 // 미디어 아이템 불러오기(앨범 포함)
 export interface GetMediaItemsReqDto {
-  page?: number; // starts with 1
+  //page?: number; // starts with 1
   sort?: OrderType;
   type?: FilterType;
   keyword?: string;
   tag?: string;
+  lastId?: string; // 마지막으로 본 아이템의 ID
+  lastValue?: string | number; // 정렬 기준이 '인기순'일 경우 마지막으로 본 좋아요 수/조회수
 }
 
 // 미디어 아이템 불러오기 응답(내 업로드, 내 좋아요 조회 시에도 사용)
